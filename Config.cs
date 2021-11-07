1f2fb69968b24d8d65f2032f53660f296831b299
JP <jpussinen@yahoo.fi>
2021-11-07 11:31:59 +0200

    Added comments and minor changes.

--- Config.cs
+++ Config.cs
@@ -1,6 +1,15 @@
 using System;
-namespace JPTask01
+
+/*
+    It is more logic to use separate namespace for the configuration.
+*/
+namespace JPTask01.Configuration
 {
+    /*
+        Exception class to throw, when configuration generation fails.
+        Not exactly needed, because we could use the base class Exception,
+        but this makes visible how exceptions can be extended and used.
+    */
     public class ConfigException : Exception 
     {
         public ConfigException(String msg)
@@ -8,10 +17,20 @@ namespace JPTask01
         {}
     }
 
-    class Config
+    /*
+        Config class logically defines application configuration.
+        It separates concerns and makes it possible to use same
+        configuration clas in different applications without rewriting
+        code.
+    */
+    public class Config
     {
         private Config() {
-            // Only factory methods can create class
+            /* 
+                Only factory methods can create class. This is convinient, because
+                then user's of the class cannot make invalid or not ready configuration
+                objects.
+            */
         }
         private static String helpMessge = @"
         This application shows pictures in a carousel.
@@ -27,16 +46,42 @@ namespace JPTask01
         private String searchDirectory;
         private String filter;
         
+        /*
+            ParseParameters is the factory function that parses comman line arguments,
+            validates them and returns valid configuration object.
+
+            If validation fails, exception is thrown. So it is impossible to create
+            invalid configuration object.
+
+            args    array of command line argument strings
+        */
         public static Config ParseParameters(string[] args) {
+            // Validata array exitence
+            if (args == null) {
+                throw new ConfigException("args is null!");
+            }
+
             Config config = new Config();
+            /*
+                Loop through parameters and validate them
+            */
             for (int i = 0; i < args.Length; ++i)
             {
+                /*
+                    There may be a library to handle command line parameters,
+                    but I have not searched for one.
+                */
                 if (args[i] == "-h" || args[i] == "--help")
                 {
+                    /*
+                        We use validation to return help message.
+                        This may be a bit unusual solution.
+                    */
                     throw new ConfigException(helpMessge);
                 } 
                 else if (args[i] == "-cf" || args[i] == "--config-file") 
                 {
+                    // Nice way of showing not (yet) implemented features.
                     throw new ConfigException("Config file is not supported!");
                 }
                 else if (args[i] == "-d" || args[i] == "--directory") 
@@ -59,6 +104,7 @@ namespace JPTask01
             if (String.IsNullOrWhiteSpace(config.searchDirectory)) {
                 throw new ConfigException("Search directory not set!");
             }
+            // After validation, we return complete and validated confgiruation object for use.
             return config;
         }
     }
--- Program.cs
+++ Program.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
-
+using JPTask01.Configuration;   // Configuration is in its own namespace
 namespace JPTask01
 {
     class Program
     {
 
+        /*
+            To get help run: jptask.exe -h or jptask.exe --help.
+        */
         static void Main(string[] args)
         {
             Config config;
             try 
             {
+                // Create valid configuration from command line parameters
                 config = Config.ParseParameters(args);
             }
             catch (ConfigException e) 
@@ -20,10 +24,26 @@ namespace JPTask01
                 Console.WriteLine(e.Message);
                 Environment.Exit(1);
             }
-            RunCMD();
+            // Here we know, that configuration is usable
+            
+            /*
+                We create a Program object and run a instance method.
+                In this way we can add (non static) fields to Program class and
+                use them in the methods. This is a bit more object oriented
+                programming style and the using static methods is a more
+                procedural style.
+
+                Not necessary, but I like this style more. 
+                Also statics are a bit bad, because the way their memory is reserved
+                at the application startup time and never relinguished (before
+                application shutdown). In long running applications that may
+                be a factor. But in many times it really does not matter.
+            */
+            Program app = new Program();
+            app.RunCMD();
         }
 
-        private static void RunCMD()
+        private void RunCMD()
         {
 
             Process process = new Process();
